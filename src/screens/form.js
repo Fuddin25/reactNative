e1e67fd66fc404edbc8533fcad772a4209f7a241
Fuddin25 <akyuc2@gmail.com>
2022-07-05 09:54:22 +0700

    update

---
+++ src/screens/form.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function List() {
+  return (
+    <View>
+      <StatusBar />
+
+    </View>
+  );
+}
+
